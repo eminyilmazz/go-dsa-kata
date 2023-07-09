4becb2247c950ed90b35387178fb2b5524979950
Emin <eminyilmzz@gmail.com>
2023-07-10 00:12:35 +0300

    test: linkedlist search(), phew

--- ds/linkedlist_test.go
+++ ds/linkedlist_test.go
@@ -6,14 +6,14 @@ import (
 )
 
 func TestLinkedList_Get(t *testing.T) {
-	list := &LinkedList{}
-	list.Append(1)
-	list.Append(420)
-	list.Append(1337)
+	l := &LinkedList{}
+	l.Append(1)
+	l.Append(420)
+	l.Append(1337)
 
 	// Test valid index
 	expected := 420
-	value, err := list.Get(1)
+	value, err := l.Get(1)
 	if err != nil {
 		t.Errorf("Expected no error, but got: %v", err)
 	}
@@ -23,7 +23,7 @@ func TestLinkedList_Get(t *testing.T) {
 
 	// Test invalid index
 	invalidIndex := uint(5)
-	_, err = list.Get(invalidIndex)
+	_, err = l.Get(invalidIndex)
 	expectedError := errors.New("index out of bounds")
 	if err == nil {
 		t.Errorf("Expected error for index %d, but got no error", invalidIndex)
@@ -33,22 +33,51 @@ func TestLinkedList_Get(t *testing.T) {
 }
 
 func TestLinkedList_Length(t *testing.T) {
-	list := &LinkedList{}
+	l := &LinkedList{}
 	expected := uint(0)
-	actual := list.Length()
+	actual := l.Length()
 
 	if actual != expected {
 		t.Errorf("Expected length %d, but got %d", expected, actual)
 	}
 
-	list.Append(1)
-	list.Append(420)
-	list.Append(1337)
+	l.Append(1)
+	l.Append(420)
+	l.Append(1337)
 
 	expected = uint(3)
-	actual = list.Length()
+	actual = l.Length()
 
 	if actual != expected {
 		t.Errorf("Expected length %d, but got %d", expected, actual)
+	}
+}
+
+func TestLinkedList_Search(t *testing.T) {
+	l := &LinkedList{}
+	needle := "420"
+	expected := -1
+	actual := l.Search(needle)
+
+	if expected != actual {
+		t.Errorf("Expected index %d, but got %d", expected, actual)
+	}
+
+	l.Append("1")
+	l.Append("420")
+	l.Append("1337")
+	expected = 1
+	actual = l.Search(needle)
+
+	if expected != actual {
+		t.Errorf("Expected index %d, but got %d", expected, actual)
+	}
+
+	needle = "help me"
+	expected = -1
+	actual = l.Search(needle)
+
+	if expected != actual {
+		t.Errorf("Expected index %d, but got %d", expected, actual)
 	}
 }
