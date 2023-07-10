1565b700e510b14e6471e12f9796c9bcc336c02a
Emin <eminyilmzz@gmail.com>
2023-07-10 21:29:33 +0300

    impl: implemented queue

---
+++ ds/queue.go
@@ -0,0 +1,31 @@
+package ds
+
+type Queue struct {
+	elements []interface{}
+}
+
+func (q *Queue) Size() int {
+	return len(q.elements)
+}
+
+func (q *Queue) IsEmpty() bool {
+	return q.Size() == 0
+}
+
+func (q *Queue) Enqueue(vars ...interface{}) {
+	q.elements = append(q.elements, vars...)
+}
+
+func (q *Queue) Dequeue() interface{} {
+	if q.Size() == 0 {
+		return nil
+	}
+
+	val := q.elements[0]
+	if q.Size() == 1 {
+		q.elements = nil
+	} else {
+		q.elements = q.elements[1:]
+	}
+	return val
+}
